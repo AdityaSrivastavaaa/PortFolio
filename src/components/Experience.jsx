5797443818c2e6e9f1723729e627b9b2bae3b5d2
AdityaSrivastavaaa <srivastavaaditya015@gmail.com>
2024-05-30 19:03:29 +0530

    updating stilll

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,14 +8,10 @@ const Experience = () => {
         <p>Robust Result Private Limited</p>
         <p className='text-gray-400'>(June 2022- July 2022)</p>
         <p className='text-gray-500'>
-          <List as='ul'>
-    <ListItem as='li'>Developed a full-stack blog website utilizing HTML, CSS, and JavaScript for the frontend, and SQL and PHP for the backend.</ListItem>
-    <ListItem as='li'>Designed and implemented an admin panel to enhance management capabilities, streamlining content moderation and user management processes.</ListItem>
-    <ListItem as='li'>
-    Gained hands-on experience in full-stack development, working on both client-side and server-side components.
-    </ListItem>
-    <ListItem as='li'>Collaborated with a team of developers to deliver a functional and user-friendly web application within a tight timeline.</ListItem>
-  </List>
+  -Developed a full-stack blog website utilizing HTML, CSS, and JavaScript for the frontend, and SQL and PHP for the backend.<br/>
+  -Designed and implemented an admin panel to enhance management capabilities, streamlining content moderation and user management processes.<br/>
+  -Gained hands-on experience in full-stack development, working on both client-side and server-side components.<br/>
+  -Collaborated with a team of developers to deliver a functional and user-friendly web application within a tight timeline.<br/>
         </p>
       </div>
       <div className='h-[50px] w-[2px] bg-slate-400 relative my-1 mx-auto'></div>
